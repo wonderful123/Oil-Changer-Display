d91c878b3db5558f2b92846c42f63886675b4f7b
Josh Jenkins <clearharry@gmail.com>
2024-01-19 14:53:26 +1000

    global config

---
+++ lib/Config/Config.cpp
@@ -0,0 +1,10 @@
+#include "Config.h"
+
+HardwarePinConfig serialConfig2({{"RXD", SERIAL_RXD_PIN},
+                                 {"TXD", SERIAL_TXD_PIN}},
+                                SERIAL_ID, "Serial",
+                                {{"uartPort", std::to_string(SERIAL_UART_PORT)},
+                                 {"baud", std::to_string(SERIAL_BAUD)}});
+
+HardwarePinConfig serialConfig({{"RXD", 36}, {"TXD", 37}}, "Serial1", "Serial",
+                               {{"uartPort", "0"}, {"baud", "115200"}});
--- lib/Config/Config.h
+++ lib/Config/Config.h
@@ -1,5 +1,14 @@
 // Config.h
 #pragma once
 
+#include "HAL/HardwarePinConfig.h"
+
 // Device Identifier
 constexpr char DEVICE_IDENTIFIER[] = "Display1";
+constexpr char SERIAL_ID[] = "Serial1";
+constexpr int SERIAL_BAUD = 115200;
+constexpr int SERIAL_RXD_PIN = 36;
+constexpr int SERIAL_TXD_PIN = 37;
+constexpr int SERIAL_UART_PORT = 0;
+
+extern HardwarePinConfig serialConfig;
