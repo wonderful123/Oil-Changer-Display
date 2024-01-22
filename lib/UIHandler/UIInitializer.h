479507965ed7a094f541a31c4909913b378a4811
Josh Jenkins <clearharry@gmail.com>
2024-01-22 15:06:16 +1000

    remove battery percentage

--- lib/UIHandler/UIInitializer.h
+++ lib/UIHandler/UIInitializer.h
@@ -11,7 +11,7 @@ extern lv_obj_t *ui_Slider_Extract;
 extern lv_obj_t *ui_Slider_Fill;
 extern lv_obj_t *ui_Fill_Capacity_Number_2;
 extern lv_obj_t *ui_Fill_Capacity_Number_1;
-extern lv_obj_t *ui_Flow_Rate_Number;;
+extern lv_obj_t *ui_Flow_Rate_Number;
 extern lv_obj_t *ui_Battery_Voltage_Number;
 extern lv_obj_t *ui_Oil_Temp_Number;
 extern lv_obj_t *ui_Amount_Filled_Number;
