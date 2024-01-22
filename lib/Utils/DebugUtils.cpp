ec1440ae480fa83f091287577ac616cc8a7aec36
Josh Jenkins <clearharry@gmail.com>
2024-01-22 12:25:25 +1000

    move log memory usage out of main

---
+++ lib/Utils/DebugUtils.cpp
@@ -0,0 +1,19 @@
+#include "DebugUtils.h"
+
+#if defined(ESP32)
+
+void DebugUtils::logMemoryUsage() {
+  Logger::debug("Total heap: " + std::to_string(ESP.getHeapSize()), __FILE__);
+  Logger::debug("Free heap: " + std::to_string(ESP.getFreeHeap()), __FILE__);
+  Logger::debug("Total PSRAM: " + std::to_string(ESP.getPsramSize()), __FILE__);
+  Logger::debug("Free PSRAM: " + std::to_string(ESP.getFreePsram()), __FILE__);
+  Logger::debug(
+      "Used PSRAM: " + std::to_string(ESP.getPsramSize() - ESP.getFreePsram()),
+      __FILE__);
+}
+#else
+// Fallback or mock implementation for non-ESP32 environments
+void DebugUtils::logMemoryUsage() {
+  Logger::debug("Memory logging not supported on this platform", __FILE__);
+}
+#endif
---
+++ lib/Utils/DebugUtils.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "DebugUtils.h"
+#include "Logger.h"
+
+/**
+ * @class DebugUtils
+ *
+ * @brief Provides a set of debugging utilities, such as memory usage logging.
+ *
+ * DebugUtils offers methods for various debugging tasks, including memory
+ * statistics logging. It uses the Logger class for outputting debug
+ * information. The implementation of some functionalities might differ based on
+ * the target platform.
+ */
+class DebugUtils {
+ public:
+  /**
+   * Logs memory usage statistics. The actual information logged may vary based
+   * on the platform.
+   */
+  static void logMemoryUsage();
+};
