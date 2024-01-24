0d2dd578fff762c743c4e91f6876818c1fa8cdbd
Josh Jenkins <clearharry@gmail.com>
2024-01-24 14:55:32 +1000

    Change element mapping names

--- lib/UIHandler/UIInitializer.cpp
+++ lib/UIHandler/UIInitializer.cpp
@@ -6,11 +6,11 @@ void UIInitializer::initialize(std::map<std::string, lv_obj_t*>& elements) {
   elements["SliderFill"] = ui_Slider_Fill;
   elements["FillCapacityNumber2"] = ui_Fill_Capacity_Number_2;
   elements["FillCapacityNumber1"] = ui_Fill_Capacity_Number_1;
-  elements["FloatRateNumber"] = ui_Flow_Rate_Number;
-  elements["BatteryVoltageNumber"] = ui_Battery_Voltage_Number;
-  elements["OilTempNumber"] = ui_Oil_Temp_Number;
-  elements["AmountFilledNumber"] = ui_Amount_Filled_Number;
-  elements["AmountExtractedNumber"] = ui_Amount_Extracted_Number;
+  elements["FlowRate"] = ui_Flow_Rate_Number;
+  elements["BatteryVoltage"] = ui_Battery_Voltage_Number;
+  elements["OilTemp"] = ui_Oil_Temp_Number;
+  elements["AmountFilled"] = ui_Amount_Filled_Number;
+  elements["AmountExtracted"] = ui_Amount_Extracted_Number;
   elements["ETATime"] = ui_ETA_Time;
   elements["RunTime"] = ui_Run_Time;
 }
