b63929188aae7a4a23498ace59a4d14111d68810
Josh Jenkins <clearharry@gmail.com>
2024-01-25 22:54:33 +1000

    initial commit

---
+++ lib/Utils/SpiRamAllocator.h
@@ -0,0 +1,18 @@
+// SpiRamAllocator.h
+#pragma once
+
+#include <ArduinoJson.h>
+
+// For ArduinoJSON
+
+struct SpiRamAllocator : ArduinoJson::Allocator {
+  void* allocate(size_t size) override {
+    return heap_caps_malloc(size, MALLOC_CAP_SPIRAM);
+  }
+
+  void deallocate(void* pointer) override { heap_caps_free(pointer); }
+
+  void* reallocate(void* ptr, size_t new_size) override {
+    return heap_caps_realloc(ptr, new_size, MALLOC_CAP_SPIRAM);
+  }
+};
