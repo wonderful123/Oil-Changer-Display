1e9eaa6791406d34958da02975edd774027a355c
Josh Jenkins <clearharry@gmail.com>
2024-01-26 07:38:11 +1000

    expand file serving

--- lib/WiFi/WebServerManager.cpp
+++ lib/WiFi/WebServerManager.cpp
@@ -21,6 +21,7 @@ void WebServerManager::initialize() {
 void WebServerManager::loadConfig() {
   LOG_INFO("Configuring WebServer...");
   auto configManager = ConfigManager::getInstance();
+
   auto webServerConfig = configManager.getWebServerConfig();
   _port = webServerConfig["port"];
   _hostname = webServerConfig["hostname"].as<std::string>();
@@ -35,6 +36,14 @@ void WebServerManager::update() { _otaUpdateManager.update(); }
 void WebServerManager::startWebServer(int port) {
   _server = std::make_shared<AsyncWebServer>(port);
   _server->serveStatic("/", LittleFS, "/www/");
+
+  _server->on("/", HTTP_GET, [this](AsyncWebServerRequest* request) {
+    request->send(LittleFS, "/index.html", "text/html");
+  });
+
+  // Generic handler for all other files
+  _server->onNotFound(
+      [this](AsyncWebServerRequest* request) { serveStaticFiles(request); });
   LOG_INFO("Started WebServer on port: " + std::to_string(_port));
 }
 
@@ -47,6 +56,10 @@ void WebServerManager::startmDNS(std::string hostname) {
 }
 
 void WebServerManager::connectToWifi(std::string ssid, std::string password) {
+  _wifiManager = std::unique_ptr<AsyncWiFiManager>(
+      new AsyncWiFiManager(_server.get(), _dnsServer.get()));
+  _wifiManager->autoConnect("Oil Change Display");
+
   WiFi.mode(WIFI_STA);  // Optional
   WiFi.begin(ssid.c_str(), password.c_str());
   while (WiFi.status() != WL_CONNECTED) {
@@ -57,16 +70,39 @@ void WebServerManager::connectToWifi(std::string ssid, std::string password) {
   Serial.println("\nConnected to the WiFi network");
   Serial.print("Local ESP32 IP: ");
   Serial.println(WiFi.localIP());
-  // _wifiManager = std::unique_ptr<WiFiManager>(
-  //     new WiFiManager(_server.get(), _dnsServer.get()));
-  // LOG_INFO("Connecting to SSID: " + ssid);
-  // _wifiManager = new ESPAsync_WiFiManager_Lite();
-  // _wifiManager->begin();
-  // _wifiManager->autoConnect(ssid.c_str(), password.c_str());
 }
 
 void WebServerManager::startOTA() { _otaUpdateManager.initialize(_server); }
 
 void WebServerManager::sendWSMessage(const std::string& message) {
   _wsManager.send(message);
+}
+
+void WebServerManager::serveStaticFiles(AsyncWebServerRequest* request) {
+  String path = request->url();
+  String mimeType = getMimeType(path);
+  if (LittleFS.exists(path)) {
+    request->send(LittleFS, path, mimeType);
+  } else {
+    request->send(404, "text/plain", "Not found");
+  }
+}
+
+const char* WebServerManager::getMimeType(const String& path) {
+  if (path.endsWith(".html"))
+    return "text/html";
+  else if (path.endsWith(".css"))
+    return "text/css";
+  else if (path.endsWith(".js"))
+    return "application/javascript";
+  else if (path.endsWith(".json"))
+    return "application/json";
+  else if (path.endsWith(".png"))
+    return "image/png";
+  else if (path.endsWith(".jpg"))
+    return "image/jpeg";
+  else if (path.endsWith(".gif"))
+    return "image/gif";
+  // add more MIME types as needed
+  return "text/plain";
 }
